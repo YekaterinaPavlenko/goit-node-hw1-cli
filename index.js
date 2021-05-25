2669a2875af721708f76007ecef2a840188941f9
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-05-25 20:29:19 +0300

    убирает лишнее

--- index.js
+++ index.js
@@ -50,32 +50,3 @@ function invokeAction({ action, id, name, email, phone }) {
 }
 
 invokeAction(argv);
-
-// program.parse(process.argv);
-
-// const argv = program.opts();
-
-// function invokeAction({ action, id, name, email, phone }) {
-//   switch (action) {
-//     case "list":
-//       contacts.listContacts();
-//       break;
-
-//     case "get":
-//       contacts.getContactById(id);
-//       break;
-
-//     case "add":
-//       contacts.addContact(name, email, phone);
-//       break;
-
-//     case "remove":
-//       contacts.removeContact(id);
-//       break;
-
-//     default:
-//       console.warn("\x1B[31m Unknown action type!");
-//   }
-// }
-
-// invokeAction(argv);
